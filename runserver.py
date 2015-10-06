b76ef2a71ca5afbfee58d3b132d7a018a0276cc6
황석진 <dollking@users.noreply.github.com>
2015-10-06 11:44:02 +0900

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -24,21 +24,23 @@ except Exception:
 pid = os.fork()
 
 if pid is not 0:
-    time.sleep(4)
+    time.sleep(2)
+    
+    dockerDir = root.replace('runserver.py', 'Dockerfiles/GradeServer_Docker')
+    os.system('sudo python ' + dockerDir + '/create_container.py')
+    
     serverDir = root.replace('runserver.py', 'pydev/GradeServer/GradeServer')
     os.chdir(serverDir)
     os.execl('/usr/bin/sudo', '/usr/bin/sudo', 'python', 'runserver.py', dbUserName, dbPassword)
 
 else:
-    dockerDir = root.replace('runserver.py', 'Dockerfiles/GradeServer_Docker')
-
-    os.system('sudo python ' + dockerDir + '/create_container.py')
-
     celeryDir = root.replace('runserver.py', 'celeryServer')
     os.chdir(celeryDir)
+    
     fp = open('data.txt', 'w')
     fp.write(dbUserName + '\n')
     fp.write(dbPassword + '\n')
     fp.close()
-    os.system('celery multi start worker -A celeryServer -l info --concurrency=1')
+    
+    os.system('celery multi start worker -A celeryServer -l info -c 1 --pidfile="./%n.pid"')
     os.system('rm -rf data.txt')
